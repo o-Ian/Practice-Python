19debc48f81284544e56408ae3994402cead654b
Ian Stigliano Silva <ianstigli@hotmail.com>
2021-04-26 16:01:44 -0300

    Update tests and exercices

---
+++ Tests/Aula17b.py
@@ -0,0 +1,7 @@
+lista = []
+
+for cont in range(0, 3):
+    lista.append(int(input('Digite um valor: 4')))
+
+for i, c in enumerate(lista):
+    print(f'Na posição {i} encontrei o valor {c}')
---
+++ Tests/Aula_17.py
@@ -0,0 +1,7 @@
+num = [0, 4, 7, 9]
+num[2] = 3
+num.append(10)
+num.insert(1, 3)
+num.pop(1)
+print(num)
+print(f'Essa lista possui {len(num)} elementos.')
